90c608563a6e34ed0b44526b51f0f457c85da38d
Dmytro <xisgame113@gmail.com>
2024-05-07 13:17:29 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import './Style.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [email, setEmail] = useState('');
+    const [messages, setMessages] = useState([]); // Добавлено состояние для отслеживания статуса отправки
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+
+    const handleSubmit = () => {
+        if (name.length === 0) {
+            alert("Name has left blank!");
+        }
+        else if (mobile.length === 0) {
+            alert("Mobile has left blank!");
+        }
+        else if (email.length === 0) {
+            alert("Email has left blank!");
+        }
+        else {
+            const url = "http://localhost/enquiry.php";
+
+            let fData = new FormData();
+            fData.append('name', name);
+            fData.append('mobile', mobile);
+            fData.append('email', email);
+
+            setIsButtonDisabled(true);
+
+            axios.post(url, fData)
+            .then(response => {
+                setMessages(response.data.split('\n'));
+                setTimeout(() => setIsButtonDisabled(false), 10000);   
+            })
+            .catch(error =>  {
+                setMessages(error); alert(error);
+                setTimeout(() => setIsButtonDisabled(false), 10000);
+            });
+
+            
+        }
+    }
+
+    return(
+        <div className='container'>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+            <label htmlFor="mobile">Mobile</label>
+            <input type="text" name="mobile" id="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="button" name="send" id="send" value="SEND" onClick={handleSubmit} disabled={isButtonDisabled} />
+            {messages.map((message, index) => (
+                <div key={index} dangerouslySetInnerHTML={{ __html: message }} />
+            ))}
+        </div>
+    )
 }
 
 export default App;
